230ea26e98fb6b7441c812e95d58bf82cfbfd346
Muhammad Abdullah <mafivs51@gmail.com>
2024-10-21 14:36:21 +0500

    AL-6 small ui fixes, signin

---
+++ almabridge_web/src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+export default function SignUp() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
