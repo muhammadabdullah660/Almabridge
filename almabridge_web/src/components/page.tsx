8aae346b14cef3bdeba14d8aa8a328a190be0fca
Muhammad Abdullah <mafivs51@gmail.com>
2024-10-22 00:10:54 +0500

    AL-6 sign in frontend

---
+++ almabridge_web/src/components/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import SignUp from "@/components/SignUp";
+export default function SignUpPage() {
+  return (
+    <div>
+      <Navbar />
+      <SignUp />
+      <Footer />
+    </div>
+  );
+}
